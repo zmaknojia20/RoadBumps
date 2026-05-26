d64fc426fb44a65cc6bdcb8076126a7f98e94dbd
Zhill Maknojia <zhillmaknojia123@gmail.com>
2026-05-25 22:42:16 -0500

    Flatten repo: inline Xcode project into root

---
+++ RoadBumps/RoadBumpsTests/RoadBumpsTests.swift
@@ -0,0 +1,18 @@
+//
+//  RoadBumpsTests.swift
+//  RoadBumpsTests
+//
+//  Created by Zhill Maknojia on 4/27/26.
+//
+
+import Testing
+
+struct RoadBumpsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
